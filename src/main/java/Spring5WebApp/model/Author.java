2e5a79a3b5160a348a107ee37f045bbf1963d13c
Andy <alipinski@mail.com>
2020-01-16 11:36:46 +0100

    added IDs, and annotations entity, id, ManyToMany

--- src/main/java/Spring5WebApp/model/Author.java
+++ src/main/java/Spring5WebApp/model/Author.java
@@ -1,14 +1,20 @@
 package Spring5WebApp.model;
 
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String firstName;
     private String lastName;
 
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
 
     public Author() {
@@ -24,6 +30,10 @@ public class Author {
         this.lastName = lastName;
         this.books = books;
     }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
 
     public String getFirstName() {return firstName;}
 
--- src/main/java/Spring5WebApp/model/Book.java
+++ src/main/java/Spring5WebApp/model/Book.java
@@ -1,13 +1,22 @@
 package Spring5WebApp.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
     private String publisher;
 
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
     public Book() {
@@ -25,6 +34,10 @@ public class Book {
         this.publisher = publisher;
         this.authors = authors;
     }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
 
     public String getTitle() {
         return title;
